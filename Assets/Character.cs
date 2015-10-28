8207f1e7f6e788fe255356ab8ea3a587ac4315a8
flip40 <john@ludeman.com>
2015-10-27 23:18:14 -0700

    basic character abstraction, needs improvement

---
+++ Assets/Character.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public interface Character {
+	// kick is for now, we should replace with generics like neutralLight, neutralHeavy, and so on
+	// the functions should describe the input that causes the move, not the move itself.
+	// neutral light would be for neutral movement (no left, right or up input) and light attack button.
+	void kick(GameObject collider, GameObject hitter);
+}
---
+++ Assets/Sanji.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sanji : MonoBehaviour, Character {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void kick(GameObject collider, GameObject hitter) {
+		collider.GetComponent<PlayerMovement>().wasThrown (hitter.transform.position.x - collider.transform.position.x);
+	}
+}
