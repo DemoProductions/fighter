ea1027787113cec1d24ddcd3e53510824028a509
flip40 <john@ludeman.com>
2015-10-27 23:24:47 -0700

    added some commentary

--- Assets/Sanji.cs
+++ Assets/Sanji.cs
@@ -14,6 +14,10 @@ public class Sanji : MonoBehaviour, Character {
 	}
 
 	public void kick(GameObject collider, GameObject hitter) {
+		// here we would apply damage unique to Sanji's kick
+		// I think we should also apply the movement vector here for the collider, current implementation is actually quite bad
+		// as the collider sets its own direction, which has nothing to do with what hit it. Didn't want to go too far on changing
+		// everything for now though.
 		collider.GetComponent<PlayerMovement>().wasThrown (hitter.transform.position.x - collider.transform.position.x);
 	}
 }
