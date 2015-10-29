eab873d9959bf2d3cc6ef49bacb2932a2021eb18
flip40 <john@ludeman.com>
2015-10-28 23:49:55 -0700

    add knockback based on kick

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -15,12 +15,15 @@ public class PlayerMovement : MonoBehaviour {
 	public bool right;
 	public bool thrown;
 	public bool thrownright;
+	public Vector2 knockbackVector;
 	public HitBoxManager hitboxmanager;
 
 	public Character character;
 	
 	public string HORIZONTAL;
 	public string VERTICAL;
+
+	public const float GRAVITY = -.05f;
 
 	// Use this for initialization
 	void Start () {
@@ -77,16 +80,16 @@ public class PlayerMovement : MonoBehaviour {
 			// from every other animation to thrown would be annoying. Thrown default returns to idle, for now.
 			anim.Play ("thrown");
 			thrown = false;
+			jumps = 1;
 		}
 
 		// Special Animation logic
 
 		// if thrown, ignore user input, velocity is away from the direction you are facing.
 		if (anim.GetCurrentAnimatorStateInfo (0).IsName ("thrown")) {
-			if(right)
-				xvelocity = -1f;
-			else
-				xvelocity = 1f;
+			xvelocity = knockbackVector.x;
+			yvelocity = knockbackVector.y;
+			knockbackVector.y -= GRAVITY;
 		}
 		// if kicking, ignore user input, don't move
 		else if (anim.GetCurrentAnimatorStateInfo (0).IsName ("kick")) {
@@ -151,9 +154,7 @@ public class PlayerMovement : MonoBehaviour {
 		}
 
 		// start character falling. Default falling felt bad. This is not necessary.
-		if (jumps > 0) {
-			yvelocity -= .05f;
-		}
+		yvelocity -= GRAVITY;
 
 		// apply movement
 		GetComponent<Rigidbody2D>().velocity = new Vector2(xvelocity, yvelocity) * Time.deltaTime * speed;
@@ -168,7 +169,9 @@ public class PlayerMovement : MonoBehaviour {
 	}
 	
 	void OnCollisionStay2D(Collision2D collision) {
-		
+		if (jumps == 0) {
+			yvelocity = 0;
+		}
 	}
 	
 	void OnCollisionExit2D(Collision2D collision) {
@@ -198,7 +201,7 @@ public class PlayerMovement : MonoBehaviour {
 	
 	public void wasThrown(float direction) {
 		// set direction to face your attacker and play thrown animation
-		if (direction > 0f) {
+		if (direction > 0) {
 			thrownright = true;
 		}
 		else {
