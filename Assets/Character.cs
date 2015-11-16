0651674e1b1b755ade66080c6594fda3dd884940
flip40 <john@ludeman.com>
2015-11-15 16:50:33 -0800

    add light attack, new colored sprites, hit animation

--- Assets/Character.cs
+++ Assets/Character.cs
@@ -3,5 +3,6 @@ using UnityEngine;
 public interface Character {
 	// the functions should describe the input that causes the move, not the move itself.
 	// neutral light would be for neutral movement (no left, right or up input) and light attack button.
+	void neutralLight(GameObject collider, GameObject hitter);
 	void neutralHeavy(GameObject collider, GameObject hitter);
 }
--- Assets/HitBoxManager.cs
+++ Assets/HitBoxManager.cs
@@ -15,7 +15,7 @@ public class HitBoxManager : ColliderManager {
 		// debug, loop, should simplify to simply the if statement and its function call
 		if (collider.gameObject.name.Contains ("HurtBox") && collider.transform.parent.name != this.transform.parent.name) {
 			reaction = "hit";
-			GetComponentInParent<PlayerMovement> ().hit (collider);
+			GetComponentInParent<PlayerMovement> ().hitCollider (collider);
 		} else {
 			reaction = "ignored";
 		}
