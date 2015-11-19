78155287312c05d9d003d4865571f7b5a109bc19
flip40 <john@ludeman.com>
2015-11-19 00:03:49 -0800

    add new collider fields to editor

--- Assets/Editor/ColliderManagerInspector.cs
+++ Assets/Editor/ColliderManagerInspector.cs
@@ -8,6 +8,9 @@ public class ColliderManagerInspector : Editor {
 		serializedObject.Update();
 		ColliderManagerList.Show(serializedObject.FindProperty("idle"));
 		ColliderManagerList.Show(serializedObject.FindProperty("run"));
+                ColliderManagerList.Show(serializedObject.FindProperty("jump"));
+                ColliderManagerList.Show(serializedObject.FindProperty("fall"));
+                ColliderManagerList.Show(serializedObject.FindProperty("land"));
 		ColliderManagerList.Show(serializedObject.FindProperty("neutralLight"));
 		ColliderManagerList.Show(serializedObject.FindProperty("neutralHeavy"));
 		serializedObject.ApplyModifiedProperties();
