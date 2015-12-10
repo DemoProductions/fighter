57a20a427ec1211cb22f3e1c96ff8d465316adb8
Adrian Lin <adrianylin@gmail.com>
2015-12-10 10:38:08 -0800

    clicking retry reloads the current scene

--- Assets/Scene.cs
+++ Assets/Scene.cs
@@ -5,23 +5,23 @@ public class Scene : MonoBehaviour {
 	public GameObject[] players;
 	public GameObject gameOver;
 	public int numPlayers;
+	public string currentLevel;
 
 	// Use this for initialization
 	void Start () {
 		PlayerSpawner playerSpawner = GameObject.Find ("PlayerSpawner").GetComponent<PlayerSpawner> ();
 		players = playerSpawner.players;
-
-		numPlayers = players.Length;
-
 		gameOver = GameObject.Find ("gameover");
+		numPlayers = players.Length;
+		currentLevel = Application.loadedLevelName;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		// for debugging purposes
-		if (Input.GetKeyDown (KeyCode.F1)) {
+		/*if (Input.GetKeyDown (KeyCode.F1)) {
 			Application.LoadLevel ("testscene");
-		}
+		}*/
 
 		if (numPlayers <= 1) { // may have to tweak this for draws
 			gameOver.GetComponent<GameOver> ().enabled = true;
